b8003778baa069aeff779ac885b2b63fc2465c8b
Crackonosh <lukashais@getgoogleoff.me>
2021-04-30 17:37:43 +0200

    Update runerror file

--- PublicIPChecker/RunError.swift
+++ PublicIPChecker/RunError.swift
@@ -11,22 +11,28 @@ enum RunError: Error {
   case FileOpenError
   case EmptyFile
   case InvalidEmail
+  case ResolverError
+  case Internal
 }
 
 //
 extension RunError {
   var code: Int {
     switch self {
-    case .InvalidNumberOfArguments:
-      return 1
-    case .NonExistingFile:
-      return 2
-    case .FileOpenError:
-      return 3
-    case .EmptyFile:
-      return 4
-    case .InvalidEmail:
-      return 5
+      case .InvalidNumberOfArguments:
+        return 1
+      case .NonExistingFile:
+        return 2
+      case .FileOpenError:
+        return 3
+      case .EmptyFile:
+        return 4
+      case .InvalidEmail:
+        return 5
+      case .ResolverError:
+        return 6
+      case .Internal:
+        return 99
     }
   }
 }
@@ -44,6 +50,10 @@ extension RunError: CustomStringConvertible {
       return "Passed file is empty!"
     case .InvalidEmail:
       return "Invalid email address found in CSV file!"
+    case .ResolverError:
+      return "Error while resolving IP address!"
+    case .Internal:
+      return "Unexpected internal error occured!"
     }
   }
 }
