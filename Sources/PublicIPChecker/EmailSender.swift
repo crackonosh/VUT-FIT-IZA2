cbe338f484865524886a7c698dab450ba50568aa
Crackonosh <lukashais@getgoogleoff.me>
2021-05-01 14:03:57 +0200

    change date formatter

--- Sources/PublicIPChecker/EmailSender.swift
+++ Sources/PublicIPChecker/EmailSender.swift
@@ -11,13 +11,9 @@ func sendEmail(old: String, new: String) -> Void {
   print("Old: " + old)
   print("New: " + new)
   // get current time and date
-  var datetime: String
-  if #available(OSX 10.12, *) {
-    datetime = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
-  } else {
-    // Fallback on earlier versions
-    datetime = "kek"
-  }
+  let frm = DateFormatter()
+  frm.dateFormat = "yyyy-MM-dd HH:mm:ssXXX"
+  let datetime = frm.string(from: Date()).replacingOccurrences(of: " ", with: "T")
   
   print(datetime)
 }
